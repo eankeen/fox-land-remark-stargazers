87ef7f1f440b7b13140cccdf0aa1d28b82f4809b
eankeen <24364012+eankeen@users.noreply.github.com>
2020-07-20 19:11:23 -0700

    style: update semis, quotes, etc.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import remark from "remark";
-import remarkInsert from "@fox-land/remark-insert";
+import remark from 'remark'
+import remarkInsert from '@fox-land/remark-insert'
 
 export interface IRemarkStarChart {
-	owner: string;
-	repo: string;
+	owner: string
+	repo: string
 }
 
 export default function remarkStarchart({ owner, repo }: IRemarkStarChart) {
 	const stargazerLink = {
-		type: "link",
+		type: 'link',
 		url: `https://starchart.cc/${owner}/${repo}`,
 		children: [
 			{
-				type: "image",
+				type: 'image',
 				url: `https://starchart.cc/${owner}/${repo}.svg`,
-				alt: "Stargazers over time",
+				alt: 'Stargazers over time',
 			},
 		],
-	};
+	}
 
 	return remarkInsert({
-		headingText: "Star Chart",
+		headingText: 'Star Chart',
 		headingDepth: 2,
 		insertionAst: stargazerLink,
-	});
+	})
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,32 +1,32 @@
-import remark from "remark";
-import remarkStargazers, { IRemarkStarChart } from "../src";
+import remark from 'remark'
+import remarkStargazers, { IRemarkStarChart } from '../src'
 
 interface Plugin {
-	fn: any;
-	options: IRemarkStarChart;
+	fn: any
+	options: IRemarkStarChart
 }
 
 export function doRemark(input: string, plugins: Plugin[]): Promise<any> {
-	let result = remark();
+	let result = remark()
 	for (const plugin of plugins) {
-		result = result().use(plugin.fn, plugin.options);
+		result = result().use(plugin.fn, plugin.options)
 	}
-	return result.process(input);
+	return result.process(input)
 }
 
 const plugins = [
 	{
 		fn: remarkStargazers,
 		options: {
-			owner: "eankeen",
-			repo: "sync-readme",
+			owner: 'eankeen',
+			repo: 'sync-readme',
 		},
 	},
-];
+]
 
 describe("work with files that have 'star chart'", () => {
-	test("ensure it works normally", async () => {
-		const input = "# Main Title\n\n## Star Chart\n\n## Conclusion\n";
+	test('ensure it works normally', async () => {
+		const input = '# Main Title\n\n## Star Chart\n\n## Conclusion\n'
 		const output = `# Main Title
 
 ## Star Chart
@@ -34,50 +34,50 @@ describe("work with files that have 'star chart'", () => {
 [![Stargazers over time](https://starchart.cc/eankeen/sync-readme.svg)](https://starchart.cc/eankeen/sync-readme)
 
 ## Conclusion
-`;
+`
 
-		const vfile = await doRemark(input, plugins);
-		console.log(vfile.contents);
-		expect(output).toBe(vfile.contents);
-	});
+		const vfile = await doRemark(input, plugins)
+		console.log(vfile.contents)
+		expect(output).toBe(vfile.contents)
+	})
 
-	test("ensure it works as last header", async () => {
-		const input = "# Main Title\n\n## Star Chart\n";
+	test('ensure it works as last header', async () => {
+		const input = '# Main Title\n\n## Star Chart\n'
 
 		const output = `# Main Title
 
 ## Star Chart
 
 [![Stargazers over time](https://starchart.cc/eankeen/sync-readme.svg)](https://starchart.cc/eankeen/sync-readme)
-`;
+`
 
-		const vfile = await doRemark(input, plugins);
-		expect(output).toBe(vfile.contents);
-	});
-});
+		const vfile = await doRemark(input, plugins)
+		expect(output).toBe(vfile.contents)
+	})
+})
 
 describe("test on files without 'star chart'", () => {
-	test("ensure it works no star chart heading", async () => {
-		const input = "# Title";
+	test('ensure it works no star chart heading', async () => {
+		const input = '# Title'
 		const output = `# Title
 
 ## Star Chart
 
 [![Stargazers over time](https://starchart.cc/eankeen/sync-readme.svg)](https://starchart.cc/eankeen/sync-readme)
-`;
+`
 
-		const vfile = await doRemark(input, plugins);
-		console.log(vfile.contents);
-		expect(output).toBe(vfile.contents);
-	});
+		const vfile = await doRemark(input, plugins)
+		console.log(vfile.contents)
+		expect(output).toBe(vfile.contents)
+	})
 
-	test("ensure it works on a blank page", async () => {
-		const input = "";
+	test('ensure it works on a blank page', async () => {
+		const input = ''
 		const output = `## Star Chart
 
 [![Stargazers over time](https://starchart.cc/eankeen/sync-readme.svg)](https://starchart.cc/eankeen/sync-readme)
-`;
-		const vfile = await doRemark(input, plugins);
-		expect(output).toBe(vfile.contents);
-	});
-});
+`
+		const vfile = await doRemark(input, plugins)
+		expect(output).toBe(vfile.contents)
+	})
+})
